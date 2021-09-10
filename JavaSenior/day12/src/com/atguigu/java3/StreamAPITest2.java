da955a23a91d4a8e96d22bc2ee3a8ff1e89231c4
Chris Jin <chris@Chriss-Mac-mini.local>
2021-09-10 11:55:06 -0400

    Java Basic Done

--- JavaSenior/day12/src/com/atguigu/java3/StreamAPITest2.java
+++ JavaSenior/day12/src/com/atguigu/java3/StreamAPITest2.java
@@ -2,18 +2,9 @@ package com.atguigu.java3;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class StreamAPITest2 {
     //1. Match and search
     @Test
     public void test1(){
-        List<Integer> l = new ArrayList<>();
-        List<Integer> l2;
-        System.out.println(l2.getClass());
-        l2 = (ArrayList<Integer>)l;
-
-        System.out.println(l2.getClass());
     }
 }
